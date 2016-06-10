378e1f77841ae6b275382372936cb28cc96476cf
SilverFox70 <wfbrinkert@gmail.com>
2016-06-10 10:09:35 -0700

    tsting sockets;

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-app.js
+express = require('express.io');
+app = express().http().io();
+
+//Broadcast all draw clicks
+app.io.route('ready', function(req){
+	req.io.broadcast('draw', req.data);
+});
+
+//Send client html
+app.get('/', function(req, res){
+	res.sendfile(_dirname + '/client.html')
+});
+
+app.listen(3000);
